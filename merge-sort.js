815e3dfa0e527276d43eb75b691e1c8de0be3a4a
jonathonlaylo <jonathon.laylo@gmail.com>
2017-02-04 19:48:09 -1000

    working merge sort w/ top down method

--- merge-sort.js
+++ merge-sort.js
@@ -1,11 +1,40 @@
 /*jshint esversion: 6 */
 
-let myArray = [9,2,5,6,4,3,7,10,1,8];
+// let myArray = [9,2,5,6,4,3,7,10,1,8,11];
+
+/*
+  top down merge sort algorithm which recursively
+  divides the input list into smaller sublists until the
+  sublists are trivially sorted, and then merges the
+  sublists while returning up the call chain.
+*/
 
 function mergeSort(myArray) {
-  // body...
+  var middle = Math.floor(myArray.length / 2);
+  var left = myArray.slice(0, middle);
+  var right = myArray.slice(middle);
+
+  if (myArray.length < 2) {
+    return myArray;
+  }
+  return merge(mergeSort(left), mergeSort(right));
 }
 
-console.log(mergeSort(myArray));
+function merge(left, right){
+  var tmp = [];
+  while(left.length && right.length){
+    if(left[0] < right[0]){
+      // shift removes the first element from an array & returns that element
+      tmp.push(left.shift());
+      // console.log('left', tmp);
+    } else {
+      tmp.push(right.shift());
+      // console.log('right', tmp);
+    }
+  }
+  return tmp.concat(left.slice().concat(right.slice()));
+}
 
-// module.exports = mergeSort;
+// console.log(mergeSort(myArray));
+
+module.exports = mergeSort;
--- test/test-script.js
+++ test/test-script.js
@@ -8,4 +8,35 @@ const mergeSort = require('../merge-sort.js');
 
 describe('The Cases of Edge', ()=>{
 
+  it('should be a function', ()=>{
+    mergeSort.should.be.a('function');
+  });
+
+  it('shoud be not be an array', () => {
+    mergeSort.should.not.be.an('array');
+  });
+
+  it('should not be a boolean',()=>{
+    mergeSort.should.not.be.a('boolean');
+  });
+
+  it('should not be a string', ()=>{
+    mergeSort.should.not.be.a('string');
+  });
+
+  it('should not be null', ()=>{
+    mergeSort.should.not.equal('null');
+  });
+
+  it('should return an array', ()=>{
+    mergeSort([9,2,5,6,4,3,7,10,1,8]).should.be.deep.equal([1,2,3,4,5,6,7,8,9,10]);
+  });
+
+  it('should not return an object', ()=>{
+    mergeSort([{}, {}, {},{}]).should.not.deep.equal([1,2,3,4,5,6,7,8,9,10]);
+  });
+
+  it('should not return the same numbers', ()=>{
+    mergeSort([1,1,1,1,1,1,1]).should.not.deep.equal([1,2,3,4,5,6,7,8,9,10]);
+  });
 });
