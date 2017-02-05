290768ee1ed2b5c06d5071ffc9a696e3043fd235
jonathonlaylo <jonathon.laylo@gmail.com>
2017-02-04 18:12:48 -1000

    created inital shell of the merge sort

---
+++ merge-sort.js
@@ -0,0 +1,11 @@
+/*jshint esversion: 6 */
+
+let myArray = [9,2,5,6,4,3,7,10,1,8];
+
+function mergeSort(myArray) {
+  // body...
+}
+
+console.log(mergeSort(myArray));
+
+// module.exports = mergeSort;
---
+++ test/test-script.js
@@ -0,0 +1,11 @@
+/*jshint esversion: 6 */
+
+const chai = require('chai');
+
+chai.should();
+
+const mergeSort = require('../merge-sort.js');
+
+describe('The Cases of Edge', ()=>{
+
+});
